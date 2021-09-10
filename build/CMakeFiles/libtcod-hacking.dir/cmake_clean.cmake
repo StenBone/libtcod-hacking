aaebba9db58c7fe3fd1d15e4453060a2bc0b8fd8
Ben Stone <spector.8500@gmail.com>
2021-09-09 21:24:34 -0400

    Initial Project Commit

---
+++ build/CMakeFiles/libtcod-hacking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libtcod-hacking.dir/main.cpp.o"
+  "libtcod-hacking"
+  "libtcod-hacking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libtcod-hacking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/libtcod-hacking-config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sten_bone/locals/libtcod-hacking
+# Build directory: /home/sten_bone/locals/libtcod-hacking/out/build/libtcod-hacking-config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
